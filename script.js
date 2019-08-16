88d2d28cf091aaccdf147d297b7928568120b4de
twogog <54079151+twogog@users.noreply.github.com>
2019-08-16 12:11:27 +0600

    upd

--- script.js
+++ script.js
@@ -1,97 +1,11 @@
-function myFunction1() {
-  var copyText = document.getElementById("myInput1");
-  copyText.select();
-  document.execCommand("copy");
-}
 
-function myFunction2() {
-  var copyText = document.getElementById("myInput2");
-  copyText.select();
-  document.execCommand("copy");
-}
-
-function myFunction3() {
-  var copyText = document.getElementById("myInput3");
-  copyText.select();
-  document.execCommand("copy");
-}
-
-function myFunction4() {
-  var copyText = document.getElementById("myInput4");
-  copyText.select();
-  document.execCommand("copy");
-}
-function myFunction5() {
-  var copyText = document.getElementById("myInput5");
-  copyText.select();
-  document.execCommand("copy");
-}
-function myFunction6() {
-  var copyText = document.getElementById("myInput6");
-  copyText.select();
-  document.execCommand("copy");
-}
-function myFunction7() {
-  var copyText = document.getElementById("myInput7");
-  copyText.select();
-  document.execCommand("copy");
-}
-function myFunction8() {
-  var copyText = document.getElementById("myInput8");
-  copyText.select();
-  document.execCommand("copy");
-}
-
-function myFunction9() {
-  var copyText = document.getElementById("myInput9");
-  copyText.select();
-  document.execCommand("copy");
-}
-function myFunction10() {
-  var copyText = document.getElementById("myInput10");
-  copyText.select();
-  document.execCommand("copy");
-}
-function myFunction11() {
-  var copyText = document.getElementById("myInput11");
-  copyText.select();
-  document.execCommand("copy");
-}
-function myFunction12() {
-  var copyText = document.getElementById("myInput12");
-  copyText.select();
-  document.execCommand("copy");
-}
-function myFunction13() {
-  var copyText = document.getElementById("myInput13");
-  copyText.select();
-  document.execCommand("copy");
-}
-function myFunction14() {
-  var copyText = document.getElementById("myInput14");
-  copyText.select();
-  document.execCommand("copy");
-}
-function myFunction15() {
-  var copyText = document.getElementById("myInput15");
-  copyText.select();
-  document.execCommand("copy");
-}
-
-function myFunction16() {
-  var copyText = document.getElementById("myInput16");
-  copyText.select();
-  document.execCommand("copy");
-}
-
-function myFunction17() {
-  var copyText = document.getElementById("myInput17");
-  copyText.select();
-  document.execCommand("copy");
-}
-
-function myFunction18() {
-  var copyText = document.getElementById("myInput18");
-  copyText.select();
-  document.execCommand("copy");
-}
+!function($){
+$(document).ready(function(){
+/* Сюда код */
+$('.click').on('click', function(){
+	let inp = $(this).prev();
+  inp.select();
+  document.execCommand('copy');
+});
+});
+}(window.jQuery);
